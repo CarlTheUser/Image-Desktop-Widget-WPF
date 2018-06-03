3d9c9009cb468c3d220a617b021f4cb41e063f7f
Carl Andres <carlandres18@gmail.com>
2018-06-03 14:40:46 +0800

    Credits updated

--- Image Desktop Widget/Image Desktop Widget/WindowBlur.cs
+++ Image Desktop Widget/Image Desktop Widget/WindowBlur.cs
@@ -11,8 +11,36 @@ namespace Image_Desktop_Widget
 {
     public static class WindowBlur
     {
+
+        #region Credits
         //Code from
         //https://github.com/riverar/sample-win10-aeroglass
+
+        //The MIT License(MIT)
+
+        //Copyright(c) 2018 Rafael Rivera
+
+        //Permission is hereby granted, free of charge, to any person obtaining a copy
+        //of this software and associated documentation files(the "Software"), to deal
+        //in the Software without restriction, including without limitation the rights
+        //to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+        //copies of the Software, and to permit persons to whom the Software is
+        //furnished to do so, subject to the following conditions:
+
+        //The above copyright notice and this permission notice shall be included in all
+        //copies or substantial portions of the Software.
+
+        //THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+        //IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+        //FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+        //AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+        //LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+        //OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+        //SOFTWARE.
+
+        //Changes Made = Made into an Extension method class
+
+        #endregion
 
         internal static void EnableBlur(this Window window)
         {
